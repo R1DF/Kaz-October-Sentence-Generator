c27e9dbfce1c3bab20ec96ea8e65ad6b8dea975a
R1DF <33981824+R1DF@users.noreply.github.com>
2022-10-23 19:10:41 +0600

    fixed input entering to remove all confusion that causes bugs

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ import random
 # Main code
 def main():
     title("Kazakh Sentence Generator (October 2022 instalment)") # Titling
-    # LOGIC ERROR WITH PAST SIMPLE
+    # LOGIC ERROR WITH PAST SIMPLE, INDEXES + ADD MORE ERROR CHECKING (3-4-5)
     # Introduction
     clear()
     print("Kazakh Sentence Generator (October '22)")
@@ -35,7 +35,7 @@ def main():
     for sentence_type_number in range(len(SENTENCE_TYPES)):
         print(f"{sentence_type_number + 1}. {SENTENCE_TYPES[sentence_type_number]} / {SENTENCE_TYPES_DESCRIPTIONS[sentence_type_number]}")
     break_line()
-    sentence_type_number = pursue_int_input("Enter sentence type by number", 1, 5) - 1
+    sentence_type_number = pursue_int_input("Enter sentence type by number", 1, 5)
 
     # Tense selection
     break_line()
@@ -44,7 +44,7 @@ def main():
         print(
             f"{tense_number + 1}. {TENSES[tense_number]} / {TENSES_DESCRIPTIONS[tense_number]}")
     break_line()
-    tense_number = pursue_int_input("Enter tense by number", 1, 8) - 1
+    tense_number = pursue_int_input("Enter tense by number", 1, 8)
 
     # Verb selection
     break_line()
@@ -57,27 +57,27 @@ def main():
     clear()
     print("Your sentence structure:")
     print("Pronoun:", PRONOUNS[pronoun_number - 1])
-    print("Mood / Type:", SENTENCE_TYPES[sentence_type_number])
-    print("Tense:", TENSES[tense_number])
+    print("Mood / Type:", SENTENCE_TYPES[sentence_type_number - 1])
+    print("Tense:", TENSES[tense_number - 1])
     print("Verb:", random_verb)
     break_line(2)
 
     await_key_press()
 
     # Making the sentence type object
-    match sentence_type_number + 1:
+    match sentence_type_number:
         case 1:
-            sentence = AbleSentence(pronoun_number, random_verb, tense_number + 1)
+            sentence = AbleSentence(pronoun_number, random_verb, tense_number)
         case 2:
-            sentence = MotivativeSentence(pronoun_number, random_verb, tense_number + 1)
+            sentence = MotivativeSentence(pronoun_number, random_verb, tense_number)
         case 3:
-            sentence = ObligativeSentence(pronoun_number, random_verb, tense_number + 1)
+            sentence = ObligativeSentence(pronoun_number, random_verb, tense_number)
         case 4:
-            sentence = PotentialSentence(pronoun_number, random_verb, tense_number + 1)
+            sentence = PotentialSentence(pronoun_number, random_verb, tense_number)
         case 5:
-            sentence = NeutralSentence(pronoun_number, random_verb, tense_number + 1)
+            sentence = NeutralSentence(pronoun_number, random_verb, tense_number)
     sentence.make_form()
-    if sentence_type_number != 2:
+    if sentence_type_number != 3:
         sentence.make_sentences()  # obligative form is simple and doesn't need this
     sentence.print_sentences()
 
